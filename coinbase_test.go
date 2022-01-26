93a18bf5e953dab3eadcf3c216886c36edcb0757
MikeOpSSH <91814777+MikeOpSSH@users.noreply.github.com>
2022-01-26 06:01:31 +0000

    remove obsolete .pot files [ci skip]

--- coinbase_test.go
+++ coinbase_test.go
@@ -10,4 +10,4 @@ func TestGet(t *testing.T) {
 	wantCoin := "SHIB"
 	wantCurrency := "EUR"
 
-	response, _ := Get(fmt.Sprintf("%s-%s", wantCoin, w
+	response, _ := Get(fmt.Sprintf("%s-%s", wantCoin, wa
