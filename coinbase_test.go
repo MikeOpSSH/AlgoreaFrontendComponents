90c0aa9195e7b9b1dc5b8d2ca94c989a2ffd9734
MikeOpSSH <91814777+MikeOpSSH@users.noreply.github.com>
2023-01-07 20:15:44 +0000

    Fix doc typo

--- coinbase_test.go
+++ coinbase_test.go
@@ -33,4 +33,4 @@ func TestGetWithDate(t *testing.T) {
 	}
 
 	if response.Currency != wantCurrency {
-		t.Errorf("TestGetWithDate() = %q, want %q", resp
+		t.Errorf("TestGetWithDate() = %q, want %q", response.C
