628027925fd19eda4480f079604f73b8d652e608
MikeOpSSH <91814777+MikeOpSSH@users.noreply.github.com>
2023-01-15 23:00:28 +0000

    breadcrumb

--- coinbase_test.go
+++ coinbase_test.go
@@ -33,4 +33,4 @@ func TestGetWithDate(t *testing.T) {
 	}
 
 	if response.Currency != wantCurrency {
-		t.Errorf("TestGetWithDate() = %q, want %q", response.Curre
+		t.Errorf("TestGetWithDate() = %q, want %q", response.Currenc
